716b730c7eedf631f17277dd8477b3340a6804a9
morenotechnology <morenotechnologycol@gmail.com>
2025-03-12 16:57:17 -0500

    Add files via upload

---
+++ src/FirstApp.jsx
@@ -0,0 +1,14 @@
+
+
+import React from "react";
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>¡Hola mundo!</h1>
+      <span>10</span>
+    </div>
+  );
+};
+
+export default FirstApp;
